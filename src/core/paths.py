d18908e8136a58cf7b2b2c849bd396f493fa4ee2
bemake-pycharm <271374667@qq.com>
2024-09-13 22:45:21 +0800

    Feature: 定义了系统核心库core，同时规范了项目路径文件paths

--- src/core/paths.py
+++ src/core/paths.py
@@ -5,5 +5,11 @@ PROJECT_DIR = Path(__file__).resolve().parent.parent.parent
 ASSETS_DIR = PROJECT_DIR / 'assets'
 OUTPUT_DIR = PROJECT_DIR / 'output'
 SCRPITS_DIR = PROJECT_DIR / 'scripts'
+UI_DIR = PROJECT_DIR / 'ui'
 SRC_DIR = PROJECT_DIR / 'src'
 RESOURCE_DIR = SRC_DIR / 'resource'
+INTERFACE_DIR = SRC_DIR / 'interface'
+
+CONFIG_FILE = PROJECT_DIR / 'config.json'
+QRC_FILE = ASSETS_DIR / 'res.qrc'
+QRC_PY_FILE = RESOURCE_DIR / 'rc_res.py'
