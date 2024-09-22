4e4e400f911a651521b6441f88e863a5db1d652f
bemake-pycharm <271374667@qq.com>
2024-09-23 00:01:03 +0800

    ✨ feat(main_view.py)：在MainView中添加插件、参数和设置视图支持 ♻️ refactor(main_view.py)：重构MainView构造函数以提高可读性和可维护性

--- src/view/main_view.py
+++ src/view/main_view.py
@@ -7,12 +7,22 @@ from src.component.cmd_text_edit import CMDTextEdit
 from src.view.basic_view import BasicView
 from src.view.embed_view import EmbedView
 from src.view.about_view import AboutView
+from src.view.args_view import ArgsView
 from src.view.advanced_view import AdvancedView
+from src.view.settings_view import SettingView
+from src.view.plugin_view import PluginView
 
 
 class MainView(FluentWindow):
     def __init__(
-        self, basic_view: BasicView,advanced_view: AdvancedView, embed_view: EmbedView, about_view: AboutView
+        self,
+        basic_view: BasicView,
+        advanced_view: AdvancedView,
+        plugin_view: PluginView,
+        embed_view: EmbedView,
+        args_view: ArgsView,
+        about_view: AboutView,
+        setting_view: SettingView,
     ):
         super().__init__()
 
@@ -20,9 +30,16 @@ class MainView(FluentWindow):
         self.basic_interface = basic_view
         self.embed_interface = embed_view
         self.advanced_interface = advanced_view
+        self.plugin_interface = plugin_view
+        self.args_interface = args_view
         self.about_interface = about_view
+        self.setting_interface = setting_view
         self.cmd_interface = CMDTextEdit()
+        
         self.about_interface.setObjectName("about")
+        self.cmd_interface.setObjectName("cmd")
+        self.plugin_interface.setObjectName("plugin")
+        self.args_interface.setObjectName("args")
         # self.setting_interface = setting_view
 
         self.initNavigation()
@@ -32,10 +49,16 @@ class MainView(FluentWindow):
         self.addSubInterface(self.basic_interface, FIF.HOME, "主页")
         self.addSubInterface(self.advanced_interface, FIF.DEVELOPER_TOOLS, "高级")
         self.addSubInterface(self.embed_interface, FIF.ZIP_FOLDER, "嵌入")
+        self.addSubInterface(self.plugin_interface, FIF.DEVELOPER_TOOLS, "插件")
+        self.addSubInterface(self.args_interface, FIF.EDIT, "参数")
         self.addSubInterface(self.cmd_interface, FIF.COMMAND_PROMPT, "日志")
 
-        self.addSubInterface(self.about_interface, FIF.CHAT, '关于', NavigationItemPosition.BOTTOM)
-        # self.addSubInterface(self.setting_interface, FIF.SETTING, '设置', NavigationItemPosition.BOTTOM)
+        self.addSubInterface(
+            self.about_interface, FIF.CHAT, "关于", NavigationItemPosition.BOTTOM
+        )
+        self.addSubInterface(
+            self.setting_interface, FIF.SETTING, "设置", NavigationItemPosition.BOTTOM
+        )
 
     def initWindow(self):
         self.resize(1100, 750)
@@ -53,6 +76,8 @@ class MainView(FluentWindow):
 
 if __name__ == "__main__":
     app = QApplication([])
-    w = MainView(BasicView(), AdvancedView(), EmbedView(), AboutView())
+    w = MainView(
+        BasicView(), AdvancedView(), PluginView(), EmbedView(), ArgsView(), AboutView(), SettingView()
+    )
     w.show()
     app.exec()
