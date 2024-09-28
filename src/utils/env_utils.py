a459864e0237ac759224ac8e198068aaf931b839
bemake-pycharm <271374667@qq.com>
2024-09-28 21:42:51 +0800

    ✨ feat(paths.py, env_utils.py)：添加多个依赖文件路径和日志记录功能 ♻️ refactor(env_utils.py)：重构环境变量处理方法，简化代码并添加调试日志

--- src/utils/env_utils.py
+++ src/utils/env_utils.py
@@ -1,15 +1,23 @@
 import os
 import winreg
 from pathlib import Path
+from typing import Optional
+
+import loguru
 
 
 class EnvUtils:
+    def __init__(self):
+        self._writeable_env_path_list: list[Path] = self.get_writeable_env()
+
     def get_writeable_env(self) -> list[Path]:
         """获取所有有写入权限的环境变量PATH中的路径"""
         with winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Environment") as key:
             current_path = winreg.QueryValueEx(key, "PATH")[0]
         paths = current_path.split(";")
-        return [Path(p) for p in paths if os.access(p, os.W_OK)]
+        result = [Path(p) for p in paths if os.access(p, os.W_OK)]
+        loguru.logger.debug(f"获取到的可写入的环境变量路径: {result}")
+        return result
 
     def add_path_to_user_env_by_os(self, dir_path: Path) -> None:
         """使用os模块将路径添加到用户环境变量
@@ -17,16 +25,7 @@ class EnvUtils:
         使用该方法添加的环境变量只在当前进程中有效, 程序结束后失效
         """
         os.environ["PATH"] = f"{dir_path};{os.environ['PATH']}"
-
-    def add_path_to_user_env_by_bat(self, dir_path: Path) -> None:
-        """使用注册表将路径添加到用户环境变量
-
-        使用该方法添加的环境变量永久有效
-        """
-        with winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Environment", 0, winreg.KEY_SET_VALUE) as key:
-            current_path = winreg.QueryValueEx(key, "PATH")[0]
-            new_path = f"{dir_path};{current_path}"
-            winreg.SetValueEx(key, "PATH", 0, winreg.REG_EXPAND_SZ, new_path)
+        loguru.logger.debug(f"向用户环境变量中添加了路径: {dir_path}")
 
     def append_path_to_user_env_by_os(self, dir_path: Path, var_name: str) -> None:
         """使用os模块将路径追加到已有的用户环境变量
@@ -35,32 +34,26 @@ class EnvUtils:
             var_name: 被追加的环境变量名
         """
         os.environ[var_name] = f"{os.environ.get(var_name, '')};{dir_path}"
-
-    def append_path_to_user_env_by_bat(self, dir_path: Path, var_name: str) -> None:
-        """使用注册表将路径追加到已有的用户环境变量
-        Args:
-            dir_path: 要追加的路径
-            var_name: 被追加的环境变量名
-        """
-        with winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Environment", 0, winreg.KEY_SET_VALUE) as key:
-            current_value = winreg.QueryValueEx(key, var_name)[0]
-            new_value = f"{current_value};{dir_path}"
-            winreg.SetValueEx(key, var_name, 0, winreg.REG_EXPAND_SZ, new_value)
+        loguru.logger.debug(f"向用户环境变量中追加了路径: {dir_path}")
 
     def remove_path_from_user_env_by_os(self, dir_path: Path) -> None:
         """使用os模块将路径从用户环境变量中移除"""
         paths = os.environ["PATH"].split(";")
         paths = [p for p in paths if p != str(dir_path)]
         os.environ["PATH"] = ";".join(paths)
+        loguru.logger.debug(f"从用户环境变量中移除了路径: {dir_path}")
 
-    def remove_path_from_user_env_by_bat(self, dir_path: Path) -> None:
-        """使用注册表将路径从用户环境变量中移除"""
-        with winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Environment", 0, winreg.KEY_SET_VALUE) as key:
-            current_path = winreg.QueryValueEx(key, "PATH")[0]
-            paths = current_path.split(";")
-            paths = [p for p in paths if p != str(dir_path)]
-            new_path = ";".join(paths)
-            winreg.SetValueEx(key, "PATH", 0, winreg.REG_EXPAND_SZ, new_path)
+    def add_file_path_to_user_env_by_bat(self, file_path: Path, target_dir_path: Optional[Path] = None) -> None:
+        """使用注册表将路径添加到用户环境变量
+
+        使用该方法添加的环境变量永久有效
+        """
+        writeable_env_path = self._writeable_env_path_list[0] if self._writeable_env_path_list else None
+        target_dir_path = writeable_env_path if target_dir_path is None else target_dir_path
+        bat_path = target_dir_path / f"{file_path.stem}.bat"
+        with open(bat_path, "w", encoding="utf-8") as f:
+            f.write(self._get_bat_cmd(str(file_path.resolve())))
+        loguru.logger.debug(f"向用户环境变量中添加了bat文件: {bat_path}")
 
     def _get_bat_cmd(self, app_name: str) -> str:
         """
@@ -75,7 +68,11 @@ class EnvUtils:
         ]
         return '\n'.join(content)
 
+
 if __name__ == '__main__':
+    from src.core.paths import GCC_EXE_FILE
+
     env_uitls = EnvUtils()
-    paths = env_uitls.get_writeable_env()
-    print(paths)
+    # paths = env_uitls.get_writeable_env()
+    # print(paths)
+    env_uitls.add_file_path_to_user_env_by_bat(GCC_EXE_FILE)
