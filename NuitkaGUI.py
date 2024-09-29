bf5593acf39f6e1d575c06ebf289c3f337d46800
bemake-pycharm <271374667@qq.com>
2024-09-30 06:23:39 +0800

    ✨ feat(NuitkaGUI.py)：添加欢迎界面以处理首次运行的情况

--- NuitkaGUI.py
+++ NuitkaGUI.py
@@ -1,12 +1,19 @@
 from PySide6.QtWidgets import QApplication
 
+from src.config import cfg
 from src.presenter.main_presenter import MainPresenter
+from src.presenter.welcome_presenter import WelcomePresenter
 
 
 def main():
     app = QApplication([])
-    main_presenter = MainPresenter()
-    main_presenter.view.show()
+    is_first_run: bool = cfg.get(cfg.is_first_run)
+    if is_first_run:
+        welcome_presenter = WelcomePresenter()
+        welcome_presenter.view.show()
+    else:
+        main_presenter = MainPresenter()
+        main_presenter.view.show()
     app.exec()
 
 
